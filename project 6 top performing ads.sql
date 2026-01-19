48f9f7ec42b80ec7b6608ef7981aa5b9c16d0483
Vaishnav G Nair <gvaishnav321@gmail.com>
2026-01-19 20:43:19 +0530

    Delete project 6 top performing ads.sql

--- project 6 top performing ads.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE ads ("Adld" INT, "Views" INT , "Clicks" INT, "Cost" DECIMAL(10,2));
-INSERT INTO public.ads VALUES (5, 1500, 120, 40.30);
-
-SELECT  "Adld", 
-  CAST("Clicks" AS DECIMAL(10,2)) / "Views" AS CTR
-FROM ads;
